8bcc68ac2a4283333e9d2cf52eb4d4e3e54b01c3
evertcolombia <evertescalante@gmail.com>
2020-09-04 15:25:48 -0500

    fixing command

--- deploy_pkg/commands/command_swarmprom_service.py
+++ deploy_pkg/commands/command_swarmprom_service.py
@@ -4,12 +4,13 @@ from commands.modules.create_connection import create_connection
 from commands.modules.init_service import init_service
 from commands.modules.init_swarmprom_service import init_swarmprom_service
 
-from typer import Argument, colors, echo, style
+from typer import Argument, Typer, colors, echo, style
 
-app = typer.Typer()
+app = Typer()
+
 
 @app.command()
-def service(ip: str = Argument(,,,, help="Server IP"),
+def service(ip: str = Argument(..., help="Server IP"),
         key_ssh: str = Argument(..., help="path to SSH key file"),
         user_ssh: str = Argument(..., help="Server User"),
         host: str = Argument(..., help="hostname for the server")):
@@ -21,6 +22,10 @@ def service(ip: str = Argument(,,,, help="Server IP"),
     server = create_connection(user_ssh, ip, key_ssh)
     init_service(host, server)
     domain = init_swarmprom_service(server)
-    entire_domain = 'https://{}'.format(domain)
-    echo(style(entire_domain, fg=colors.BLUE, bold=True))
-
+    msg = "Can test the swarmprom stack in the follow subdomain:"
+    echo(style(msg, fg=colors.BLUE, bold=True))
+    msg = "https://{}.{}"
+    echo(style(msg.format('grafana', domain), fg=colors.BLUE, bold=True))
+    echo(style(msg.format('altermanager', domain), fg=colors.BLUE, bold=True))
+    echo(style(msg.format('unsee', domain), fg=colors.BLUE, bold=True))
+    echo(style(msg.format('prometheus', domain), fg=colors.BLUE, bold=True))
