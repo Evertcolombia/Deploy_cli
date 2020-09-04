c0e296c93bbaf2e985322d7414d779f9289f7863
evertcolombia <evertescalante@gmail.com>
2020-09-04 01:30:13 -0500

    command to init portainer service

---
+++ deploy_pkg/commands/command_portainer_service.py
@@ -0,0 +1,26 @@
+#!/bin/env python3
+
+from commands.modules.create_connection import create_connection
+from commands.modules.init_portainer_service import init_portainer_service
+from commands.modules.init_service import init_service
+
+import typer
+from typer import Argument, colors, echo, style
+
+app = typer.Typer()
+
+@app.command()
+def service(ip: str = Argument(..., help="Server IP"),
+        key_ssh: str = Argument(..., help="Path to SSH key file"),
+        user_ssh: str = Argument(..., help="Server User"),
+        host: str = Argument(..., help="hostname for the server"),):
+    """
+        Portainer user interface allows you to see
+        the state of your Docker services in a Docker
+        Swarm mode cluster and manage it.
+    """
+    server = create_connection(user_ssh, ip, key_ssh)
+    init_service(host, server)
+    domain = init_portainer_service(server)
+    entire_domain = 'https://{}'.format(domain)
+    echo(style(entire_domain, fg=colors.BLUE, bold=True))
